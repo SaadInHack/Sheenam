423af06754531973baf72bb6aca54ab7e0c7e5a9
Mrlegend08 <muslimdasturchi@gmail.com>
2023-06-22 22:16:34 +0500

    DATA: Model & Migrate Guest

---
+++ Sheenam.Api/Brockers/Storages/StorageBrocker.cs
@@ -0,0 +1,32 @@
+﻿//==================================================
+// Copyright (c) Coalation of Good-Hearted Engineers
+// Free to Use Comfort And Peace
+//==================================================
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Sheenam.Api.Brockers.Storages
+{
+    public partial class StorageBrocker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBrocker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString =
+                this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public override void Dispose(){}
+    }
+}
---
+++ Sheenam.Api/Moduls/Foundations/Guests/GenderType.cs
@@ -0,0 +1,14 @@
+﻿//==================================================
+// Copyright (c) Coalation of Good-Hearted Engineers
+// Free to Use Comfort And Peace
+//==================================================
+
+namespace Sheenam.Api.Moduls.Foundations.Guests
+{
+    public enum GenderType
+    {
+        Mail,
+        Female,
+        Other
+    }
+}
