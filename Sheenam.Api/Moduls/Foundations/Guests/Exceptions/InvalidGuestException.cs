a7a747be534967fbacc8e6e298f862f3a5b01895
Mrlegend08 <muslimdasturchi@gmail.com>
2023-06-25 11:30:56 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsInvalidAndLogItAsync => FAIL

---
+++ Sheenam.Api/Moduls/Foundations/Guests/Exceptions/InvalidGuestException.cs
@@ -0,0 +1,16 @@
+﻿//==================================================
+// Copyright (c) Coalation of Good-Hearted Engineers
+// Free to Use To Find Comfort And Peace
+//==================================================
+
+using Xeptions;
+
+namespace Sheenam.Api.Moduls.Foundations.Guests.Exceptions
+{
+    public class InvalidGuestException : Xeption
+    {
+        public InvalidGuestException()
+            :base(message: "Guest is invalid")
+        {}
+    }
+}
