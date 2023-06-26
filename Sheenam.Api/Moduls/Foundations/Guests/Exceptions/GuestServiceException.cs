de64256ab6a0c45ec7b0a2c9092973f1ed29c496
Mrlegend08 <muslimdasturchi@gmail.com>
2023-06-26 10:00:52 +0500

    ShouldThrowServiceExceptionOnAddIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api/Moduls/Foundations/Guests/Exceptions/GuestServiceException.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalation of Good-Hearted Engineers
+// Free to Use To Find Comfort And Peace
+//==================================================
+
+using Xeptions;
+
+namespace Sheenam.Api.Moduls.Foundations.Guests.Exceptions
+{
+    public class GuestServiceException : Xeption
+    {
+        public GuestServiceException(Xeption innerException)
+            :base(message: "Guest service error occurred, contact support", 
+                 innerException)
+        {}
+    }
+}
