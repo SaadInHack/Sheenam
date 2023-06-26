8217dfff5338209aa1b34af19888d9a6cb849784
Mrlegend08 <muslimdasturchi@gmail.com>
2023-06-26 08:45:44 +0500

    ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exception.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exception.cs
@@ -3,6 +3,7 @@
 // Free to Use To Find Comfort And Peace
 //==================================================
 
+using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
 using Moq;
 using Sheenam.Api.Moduls.Foundations.Guests;
@@ -48,7 +49,46 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
 
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync()
+        {
+            // given
+            Guest someGuest = CreateRandomGuest();
+            string someMessage = GetRandomString();
 
+            var duplicateKeyException =
+                new DuplicateKeyException(someMessage);
+
+            var alreadyExistGuestException =
+                new AlreadyExistGuestException(duplicateKeyException);
+
+            var expectedGuestDependencyValidationException = 
+                new GuestDependencyValidationException(alreadyExistGuestException);
+
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(someGuest))
+                .ThrowsAsync(duplicateKeyException);
+            // when
+            ValueTask<Guest> addGuestTask =
+                this.guestService.AddGuestAsync(someGuest);
+
+            // then
+            await Assert.ThrowsAsync<GuestDependencyValidationException>(() =>
+                addGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertGuestAsync(someGuest),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
---
+++ Sheenam.Api/Moduls/Foundations/Guests/Exceptions/AlreadyExistGuestException.cs
@@ -0,0 +1,17 @@
+﻿//==================================================
+// Copyright (c) Coalation of Good-Hearted Engineers
+// Free to Use To Find Comfort And Peace
+//==================================================
+
+using System;
+using Xeptions;
+
+namespace Sheenam.Api.Moduls.Foundations.Guests.Exceptions
+{
+    public class AlreadyExistGuestException : Xeption
+    {
+        public AlreadyExistGuestException(Exception innerException)
+            :base(message: "Guest already exist", innerException)
+        {}
+    }
+}
